139198dcc6f322333d31ab54fabe2b951bfb1dd4
Denis Gitau <denisgitau12@gmail.com>
2023-07-03 16:19:41 +0300

    Configure cors in app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const cors = require("cors");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const v1MemberRoutes = require("./v1/routes/memberRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+app.use(cors());
+
+app.use(express.json());
 
 app.use("/api/v1/workouts", v1WorkoutRouter);
 app.use("/api/v1/members", v1MemberRoutes);
